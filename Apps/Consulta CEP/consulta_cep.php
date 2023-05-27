3d0d4b90a8a4f148e9241020337568a0b72056c2
= <=>
2023-05-27 14:08:49 -0300

    Index styiling

--- Apps/Consulta CEP/consulta_cep.php
+++ Apps/Consulta CEP/consulta_cep.php
@@ -1,34 +1,23 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<style>
-			.container {
-				width: 40%;
-				margin: auto;
-				box-shadow: 0 0 10px #000;
-				padding: 2% 10%;
-			}
-			.res {
-
-			}
-		</style>
-		
+		<link rel="stylesheet" href="../_static/stylo.css">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 	</head>
-	<body>
+	<body class="bg-dark">
 		<div class="container">
 
 		<div class="assets d-flex">
-			<h1 class="justify-center"> Consultar CEP: </h1>
+			<h1 class="justify-center text-light"> Consultar CEP: </h1>
 			<input type="text" class="cep mx-4">
 			
 			<button class="send">Pesquisar Cep</button>
 		</div>
 
 			<!-- Resultado da Requisição -->
-			<div class="end_res"></div>
+			<div class="end_res text-light"></div>
 			<br/>
-			<div class="ngh_res"></div>
+			<div class="ngh_res text-light"></div>
 		</div>
 		
 		<script type="text/javascript">
---
+++ Apps/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="_static/stylo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title> Índice </title>
+</head>
+<body class="">
+    <div class="contt rounded w-40 mx-auto">
+        <h1 class="title glow-text text-center text-light justify-center mb-4"> Aplicativos </h1>
+
+        <!-- List with Bootstrap -->
+        <ul class="d-none glow list-group text-light mt-5 mb-5">
+            <li class="list-group-item"> <a class="list-group-item" href="consulta cep/consulta_cep.php"> Consulta CEP </a> </li>
+            <li class="list-group-item"> <a class="list-group-item" href="login panel/login_panel.php">Painel de Login</a> </li>
+        </ul>
+
+        <!-- Raw List -->
+        <ul class="items-list rounded">
+            <a class=" item-i text-center p-4" href="consulta cep/consulta_cep.php"> Consulta CEP </a>
+            <a class=" item-i text-center p-4" href="login panel/login_panel.php">Painel de Login</a>
+        </ul>
+    </div>
+</body>
+</html>
